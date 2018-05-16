3c438f43d0e79f45d01f12997872bc20f4ecbdcb
Zachary Prisk <zachary.prisk@gmail.com>
2018-05-15 21:32:12 -0500

    Adds STATUS_UNAUTHORIZED.

--- src/Priskz/Payload/PayloadInterface.php
+++ src/Priskz/Payload/PayloadInterface.php
@@ -4,14 +4,15 @@ namespace Priskz\Payload;
 
 interface PayloadInterface
 {
-	const STATUS_VALID     = 'valid';
-	const STATUS_INVALID   = 'invalid';
-	const STATUS_CREATED   = 'created';
-	const STATUS_UPDATED   = 'updated';
-	const STATUS_DELETED   = 'deleted';
-	const STATUS_FOUND     = 'found';
-	const STATUS_NOT_FOUND = 'not_found';
-	const STATUS_EXCEPTION = 'exception';
+	const STATUS_VALID        = 'valid';
+	const STATUS_INVALID      = 'invalid';
+	const STATUS_CREATED      = 'created';
+	const STATUS_UPDATED      = 'updated';
+	const STATUS_DELETED      = 'deleted';
+	const STATUS_FOUND        = 'found';
+	const STATUS_NOT_FOUND    = 'not_found';
+	const STATUS_EXCEPTION    = 'exception';
+	const STATUS_UNAUTHORIZED = 'unauthorized';
 
 	/**
 	 *	Get Data
